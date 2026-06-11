c1692fbf25bbc76bc22a1edb7268cd09d6f26070
itc-n26023 <n26023@std.it-college.ac.jp>
2026-06-11 04:58:18 +0000

    README.mdに第6章を追記

--- chapter04/challenge4-05.py
+++ chapter04/challenge4-05.py
@@ -1,9 +1,4 @@
 def f(x):
-    """
-    引数をfloat型に変換する関数
-    :x: str: 変換したい文字列
-    :戻り値: float型に変換された値
-    """
     try:
         return float(x)
     except ValueError:
@@ -11,4 +6,3 @@ def f(x):
 
 ans = f("3.14")
 print(ans)
-
